abd62c577e8e6bb189e60f8b60202cb867636625
juln <zjl5@meitu.com>
2023-08-23 11:29:36 +0800

    feat: 新增PromiseFn和PromisableFn

--- src/type-fest/function.d.ts
+++ src/type-fest/function.d.ts
@@ -1,7 +1,7 @@
-export type PromiseFn<T extends Function> = (
+export type PromiseFn<T extends (...args: any) => any> = (
   ...args: Parameters<T>
 ) => Promise<ReturnType<T>>;
 
-export type PromisableFn<T extends Function> = (
+export type PromisableFn<T extends (...args: any) => any> = (
   ...args: Parameters<T>
 ) => Promise<ReturnType<T>> | ReturnType<T>;

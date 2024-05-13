9f97d518f484b3e2c169e26611c5961aa10c9561
juln <zjl5@meitu.com>
2024-05-13 19:29:46 +0800

    fix

--- src/type-fest/object.d.ts
+++ src/type-fest/object.d.ts
@@ -35,23 +35,6 @@ type InnerDeepKeysOfSpreadable<
  * type Keys = DeepKeysOfObjectFormula<Obj>; // `Keys` equivalent to 0 | 'c' | 'd' | 'f' | Symbol.hasInstance;
  */
 export type DeepKeysOfSpreadable<Obj extends Record<any, any>> = InnerDeepKeysOfSpreadable<Obj>;
-import { ValueOf } from 'type-fest';
-
-type TupleOrArray = readonly [...unknown[]];
-
-/**
- * An object or an array.
- */
-export type Spreadable = object | TupleOrArray;
-
-type InnerDeepKeysOfSpreadable<
-  Obj extends Record<any, any>,
-  Keys extends string | number | symbol = keyof Obj,
-> = ValueOf<{
-  [K in Keys]: Obj[K] extends Spreadable
-    ? InnerDeepKeysOfSpreadable<Obj[K]>
-    : K;
-}>;
 
 /**
  * @example
